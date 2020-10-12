c84b46cbdadc8e24e10e667f0dd44e078fa57124
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-10-12 13:58:03 +0300

    1
    1

--- build/js/main.js
+++ build/js/main.js
@@ -6,12 +6,33 @@ $(function(){
 		centerMode: true,
 		arrows: false,
 		asNavFor: '.tovar-elem__sliderbot',
+		responsive: [
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        dots: true,
+		      }
+		    },
+		]
 	})
 
 	$('.tovar-elem__sliderbot').slick({
 		slidesToShow: 1,
 		asNavFor: '.tovar-elem__slider',
 		focusOnSelect: true,
-		variableWidth: true
+		variableWidth: true,
+		infinite: false,
 	})
+	$(window).on('load resize', function(){
+	  if ($(window).width() <= 1199) {
+	  	$('.tovar-elem__line1').insertBefore('.tovar-elem__left');
+	    $('.tovar-elem__mame').insertBefore('.tovar-elem__line1');
+	  }
+	});
+
+	$('.tovar-buy-bnt').click(function(){
+		$(this).hide()
+		$('.tovar-buy-bnt.link').css('display', 'flex')
+	})
+
 })
